d79fc74fdecfc60e2da37d6a520fa8a6c9d42acb
MiriamSantos2023 <UG_TALENTOS_C08100O@asav.brm>
2023-04-27 08:55:17 -0300

    subindo o exercício de operadores main

---
+++ index.js
@@ -0,0 +1,71 @@
+// console.log("hello")
+// // comentario
+// //prompt("Digite seu nome?")
+
+// const nome = prompt ("digite seu nome")
+// const idade = prompt("Digite sua idade")
+// const endereço = prompt("digite seu endereço?")
+// console.log(nome)
+// console.log(idade)
+// console.log(endereço)
+// const nome = prompt ("digite seu nome")
+// const idade = prompt ("digite sua idade")
+// console.log("Olá!", "Meu nome é", nome, "e eu tenho", idade, "anos")
+// const nome = prompt ("digite seu nome")
+// const sobrenome = prompt ("digite seu sobrenome")
+// const idade = prompt ("digite sua idade")
+// const estudante = prompt ("Você é estudante? (sim / Não) ?") 
+// console.log ("Nome" , sobrenome, "Idade:" , idade, "Estudante:" , estudante)
+// const got = "Game of Thrones"
+// const temporadas = 8
+// console.log(typeof Got)
+// console.log(typeof temporadas)
+// let idade = Number (prompt)
+
+// << Análise 1 >>
+// let a = 10
+// let b = 10
+
+// console.log(b)
+
+// b = 5
+// console.log(a, b)
+// //console (b) atribuiu que b equivale a 5
+
+// << Análise 2>>
+// let a = 10
+// let b = 20
+// c = a
+// b = c
+// a = b
+
+// console.log(a, b, c)
+// com o let a e b foi atribuído o valor de 10  mas lembrando que sem o let na frente é como se existisse o LET 
+//let horas = prompt("Quantas horas você trabalha por dia?")
+//let valor = prompt("Quanto você recebe por dia?")
+//alert(`Voce recebe ${horas/valor} por hora`)
+
+
+// let nome
+// let Idade
+
+// nome = String(prompt("digite seu nome"))
+// idade = Number(prompt ("Digite sua idade"))
+
+// console.log(typeof nome)
+// console.log(typeof idade)
+// console.log("Olá," , nome, "você tem" , idade, "anos")
+
+// const país = prompt ("Você mora no Brasil? (sim / não) ?")
+// const cidade = 
+// let horas = prompt("Quantas horas você trabalha por dia?")
+
+let a = Number (prompt ("digite um numero"))
+let b = Number ("digite outro numero")
+let c = b
+b = a
+a = c
+console.log("Valor do primeiro numero" ,a);
+console.log("valor do segundo numero" ,b);
+
+
